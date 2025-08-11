65b1f72386915463e9f98f6004a66870548bf151
maskharisajah <maskharisajah@gmail.com>
2025-08-11 10:42:21 +0700

    learn query keys

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ interface IProduct {
 export default function Home() {
   const [showToast, setShowToast] = useState(false);
   const [fetchData, setFetchData] = useState(false);
+  const [showProduct, setShowProduct] = useState<string | null>(null);
   const { data, isLoading, isError, isSuccess } = useQuery({
     queryKey: ["products"],
     queryFn: async () => {
@@ -20,6 +21,24 @@ export default function Home() {
     },
     enabled: fetchData,
   });
+
+  const {
+    data: detailProduct,
+    isLoading: isLoadingDetailProduct,
+    isError: isErrorDetailProduct,
+    isSuccess: isSuccessDetailProduct,
+  } = useQuery({
+    queryKey: ["product", showProduct],
+    queryFn: async () => {
+      const res = await fetch(
+        `https://fakestoreapi.com/products/${showProduct}`
+      );
+      return res.json();
+    },
+    enabled: showProduct !== null,
+  });
+
+  console.log(detailProduct);
 
   useEffect(() => {
     if (isSuccess) {
@@ -67,6 +86,7 @@ export default function Home() {
                 <div
                   key={`product-${product.id}`}
                   className='shadow p-4 flex flex-col items-center'
+                  onClick={() => setShowProduct(product.id)}
                 >
                   <Image
                     width={100}
@@ -85,6 +105,30 @@ export default function Home() {
           )}
         </div>
       )}
+      <div
+        className={`fixed h-screen w-screen top-0 left-0 bg-black/50 ${
+          showProduct ? "flex justify-center items-center" : "hidden"
+        }`}
+      >
+        <div className='relative w-1/2 h-1/2 bg-white flex items-center gap-8 p-8'>
+          <button
+            className='absolute top-5 right-5'
+            onClick={() => setShowProduct(null)}
+          >
+            X
+          </button>
+          <img
+            src={detailProduct?.image}
+            alt={detailProduct?.title}
+            className='w-1/4'
+          />
+          <div className='w-3/4 space-y-4 h-3/4  overflow-auto'>
+            <h1 className='font-bold text-2xl'>{detailProduct?.title}</h1>
+            <p className='text-md '>{detailProduct?.description}</p>
+            <p className='text-2xl font-bold'>{detailProduct?.price}$</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
